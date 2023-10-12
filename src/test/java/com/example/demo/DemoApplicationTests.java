79f8c7d517412a722947b5f996fe00b2532a6918
Andrii <380679724615@mail.ru>
2023-10-12 22:58:46 +0200

    Initial commit

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplicationTests.class);
+	private
 	@Test
 	void contextLoads() {
+		logger.debug("Hi it's a successful test");
+		assertTrue(true);
 	}
 
 }
